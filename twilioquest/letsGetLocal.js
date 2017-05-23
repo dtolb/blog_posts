8932e55b1403364566fb11b38f9dd05ecbd33667
Dan Tolbert <dtolbert@bandwidth.com>
2017-05-23 11:50:19 -0700

    updates

--- twilioquest/letsGetLocal.js
+++ twilioquest/letsGetLocal.js
@@ -8,45 +8,45 @@ const history = 'Did you know my favorite historical figure, Obi Wan Kenobi, die
 const historyPic = 'https://vignette4.wikia.nocookie.net/starwars/images/4/4e/ObiWanHS-SWE.jpg';
 
 const print = (a) => {
-	console.log(a);
+    console.log(a);
 };
 
 const getNumbers = () => {
-	const optionDefinitions = [
-		{ name: 'number', type: String, multiple: true, defaultOption: true }
-	];
-	return commandLineArgs(optionDefinitions).number;
+    const optionDefinitions = [
+        { name: 'number', type: String, multiple: true, defaultOption: true }
+    ];
+    return commandLineArgs(optionDefinitions).number;
 };
 
 const getTime = () => {
-	const dateTime = require('node-datetime');
-	let dt = dateTime.create().format('Y-m-d H:M:S');
-	return `Greetings! The current time is: ${dt}`
+    const dateTime = require('node-datetime');
+    let dt = dateTime.create().format('Y-m-d H:M:S');
+    return `Greetings! The current time is: ${dt}`
 };
 
 const sendTime = (number) => {
-	return client.messages.create({
-		body: getTime(),
-		to: number,  // Text this number
-		from:  TWILIO_NUMBER// From a valid Twilio number
-	})
+    return client.messages.create({
+        body: getTime(),
+        to: number,  // Text this number
+        from:  TWILIO_NUMBER// From a valid Twilio number
+    })
 };
 
 const sendHistory = (number) => {
-	return client.messages.create({
-		body: history,
-		to: number,  // Text this number
-		mediaUrl: historyPic,
-		from:  TWILIO_NUMBER// From a valid Twilio number
-	})
+    return client.messages.create({
+        body: history,
+        to: number,  // Text this number
+        mediaUrl: historyPic,
+        from:  TWILIO_NUMBER// From a valid Twilio number
+    })
 };
 
 const main = async () => {
-	const numbers = getNumbers();
-	for (let number of numbers) {
-		await sendTime(number);
-		await sendHistory(number);
-	};
+    const numbers = getNumbers();
+    for (let number of numbers) {
+        await sendTime(number);
+        await sendHistory(number);
+    };
 };
 
 main();
